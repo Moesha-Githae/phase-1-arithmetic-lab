27cd7ab8a9707a1556f8d97309d154fd0e25832b
Moesha-Githae <moesha.githae@student.moringaschool.com>
2023-03-14 14:37:38 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 
 // Write your code here
+function multiply(){
+
+    const num1 = 31 ;
+    const num2 =  2 ;
+  
+    return num1 * num2 
+}
+
+function random() {
+
+    Math.floor(Math.random() * 10) + 1;
+}
+
+function mod(){
+    const num3 = 68;
+    const num4 = 8;
+
+return  num3 % num4;
+}
+function max(){
+
+Math.max(5,6,20,19,11,12)
+
+}
